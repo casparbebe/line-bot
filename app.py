6d6e3db4cbf2dc497d3322e98bf9d55dac04ceff
Caspar Chang <caspar2201@gmail.com>
2021-07-21 14:42:47 +0800

    Update

--- app.py
+++ app.py
@@ -42,13 +42,13 @@ def handle_message(event):
 
     if '貼圖' in msg:
         sticker_message = StickerSendMessage(
-        package_id='1',
-        sticker_id='1'
+        package_id='6359',
+        sticker_id='11069851'
         )
 
         line_bot_api.reply_message(
-        event.reply_token, 
-        sticker_message)
+            event.reply_token, 
+            sticker_message)
         return
 
 
